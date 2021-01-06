36c4aa17c2da521f7a5f5c4e72f24291911f3c7f
hk147159 <71679844+hk147159@users.noreply.github.com>
2021-01-06 13:04:46 +0800

    測試上傳

---
+++ VR_End_105051006/Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public void RestartGame()
+    {
+        SceneManager.LoadScene("VR 場景");
+    }
+
+
+    public void QuitGame()
+    {
+
+        Application.Quit(); 
+    
+    }
+
+
+}
+ 
---
+++ VR_End_105051006/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    [Header("金幣音效")]
+    public AudioClip soundCoin;
+
+    private AudioSource aud;
+    private void Awake()
+
+       
+    {
+       aud = GetComponent<AudioSource>();
+    }
+
+    void OnCollisionEnter(Collision collision)
+        
+    {
+
+        if (collision.gameObject.name == "Drawers")
+        {
+
+           aud.PlayOneShot(soundCoin, 1.5f);
+
+        }
+
+    }
+
+
+
+
+
+
+}
